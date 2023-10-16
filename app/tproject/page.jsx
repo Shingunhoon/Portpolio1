f87fcf1068384791f412c14fca3364ab8714e98c
geonhoon <rjsgns01@naver.com>
2023-10-16 12:00:22 +0900

    first commit

--- app/tproject/page.jsx
+++ app/tproject/page.jsx
@@ -6,7 +6,13 @@ const ProjectPage = () => {
       <h1 className="text-4xl mb-10 font-bold">팀 프로젝트</h1>
       <div className="mb-10 bg-blue-100 p-4 m-4 rounded-lg ">
         <h1 className="text-3xl">1st</h1>
-        <Image src="/images/p1.jpg" width={500} height={500} />
+        {/* <Image src="/images/p1.jpg"
+        width={500} height={500} />
+         */}
+        <a href="https://potato-market.vercel.app/">
+          <img src="/images/p1.jpg" />
+        </a>
+
         <p> 92015075 김평안</p>
         <p> 92014966 김건희</p>
         <p> 92015154 박준형</p>
