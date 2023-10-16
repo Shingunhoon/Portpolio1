53f53a514addac2ce43228596f71473c7b2c9977
geonhoon <rjsgns01@naver.com>
2023-10-16 12:00:22 +0900

    first commit

--- app/pproject/page.jsx
+++ app/pproject/page.jsx
@@ -4,9 +4,25 @@ const ProjectPage = () => {
   return (
     <>
       <h1 className="text-4xl mb-10 font-bold">개인 프로젝트</h1>
-      <div className="mb-10 bg-blue-100 p-4 m-4 rounded-lg">1st</div>
+      <div className="mb-10 bg-blue-100 p-4 m-4 rounded-lg text-3xl">
+        1st{" "}
+        <Link
+          href="https://sgh01.vercel.app/"
+          className="text-xl text-blue-700 font-bold"
+        >
+          https://sgh01.vercel.app/
+        </Link>
+      </div>
 
-      <div className="mb-10 bg-blue-100 p-4 m-4 rounded-lg">2nd</div>
+      <div className="mb-10 bg-blue-100 p-4 m-4 rounded-lg text-3xl">
+        2nd{" "}
+        <Link
+          href="https://second-kohl.vercel.app/"
+          className="text-xl text-blue-700 font-bold"
+        >
+          https://sgh01.vercel.app/
+        </Link>
+      </div>
 
       <div className="mb-10 bg-blue-100 p-4 m-4 rounded-lg">
         <p className="">3rd</p>
